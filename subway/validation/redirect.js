f682c50d0d0ae35252b2fb9b9efea7450ab36469
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2023-05-15 14:59:47 -0600

    Update redirect.js
    Update with previous version that re-assigns urls to 404.html page.

--- subway/validation/redirect.js
+++ subway/validation/redirect.js
@@ -1,18 +1,20 @@
-window.addEventListener("load", disableSearch);
-const linkClick = document.querySelector("a");
+$( document ).on( "wb-ready.wb", function( event ) {
 
-linkClick.addEventListener("click", reDirect);
+//Renders search disabled
+$('#wb-srch-sub').attr("disabled", true);
 
-function disableSearch() {
-const trgtInputs = document.querySelector("#wb-srch-q, #wb-srch-sub").setAttribute('disabled', true);
-for (const trgtInput of trgtInputs) {
-trgtInput.setAttribute('disabled', true);
-	}
-}
+//Seperate script to null all canada.ca links to a 404 page (including GC menu) and redirect some in menu
+	//custom redirects
+	//$("a[href='urlonpage']").attr("href", "urltoredirect");
 
-function reDirect(e) {
-    if(linkClick.href.indexOf('canada.ca') > -1) { 
-        e.preventDefault();
-      window.location.href = '/cra-arc/subway/validation/404.html';
-	}
-}
+//Redirect of government pages	
+$("a[href*='canada.ca']").attr("href", "/cra-arc/subway/validation/404.html");
+$("a[href*='.gc.ca/']").attr("href", "/cra-arc/subway/validation/404.html");
+//});
+
+//Remove visited link design from 404 pages
+var visited_link_styling = "<style> a[href*='404.html']:visited{ color:#284162; } </style>"; $('head').append( visited_link_styling );
+//Hiding menu and search from navigation
+//$(".gcweb-menu>.container, #wb-srch").addClass("hidden");
+});
+
