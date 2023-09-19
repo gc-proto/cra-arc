d1c06b8d64232609c4d9c4f8f82eee13f31f1764
Christopher-O <christopher.oakes@cra-arc.gc.ca>
2023-09-19 08:04:13 -0600

    Create gloabal-menu.js

---
+++ gcweb-proposals/gloabal-menu/assets/gloabal-menu.js
@@ -0,0 +1,58 @@
+//Function to add title and menu of guide and footer
+"use strict";
+var index = [];
+if (document.documentElement.lang === "en") {
+	index = ["menu-include-en.html", "footer-include-en.html"];
+}
+if (document.documentElement.lang === "fr") {
+	index = ["menu-include-fr.html", "footer-include-fr.html"];
+}
+var injectSpot = ["menu-include", "footer-include"];
+var request = new XMLHttpRequest();
+
+document.addEventListener('DOMContentLoaded', () => {
+	(function loop(i, length) {
+		if (i>= length) {
+			return;
+		}
+		var url = index[i];
+
+		request.open("GET", url, true);
+		request.onload = () => {
+			if(request.readyState === XMLHttpRequest.DONE)
+			{
+				if (request.status === 200) {
+					document.getElementById(injectSpot[i]).innerHTML = request.responseText;
+					loop(i + 1, length);
+				}
+			}
+		};
+		request.send(null);
+	})(0, index.length);
+});
+//function to create inline overlay and to delay button switch to close menu (due to propagation in close event that cannot be properly stopped by JS
+//script also pre-emptively opens details menu by default
+
+window.onload = function() {
+
+	$(document).on( "opened.wb-overlay", "#mb-pnl", function() {
+		const myTimeout = setTimeout(showClose, 200);
+		function showClose() {
+			$("#mb-tggl-open, #mb-tggl-close").toggle();
+		}
+		console.log("it's open");
+	});
+
+	$(document).on( "closed.wb-overlay", "#mb-pnl", function() {
+		const myTimeout = setTimeout(showOpen, 200);
+		function showOpen() {
+			$("#mb-tggl-close, #mb-tggl-open").toggle();
+		}
+		console.log("it's closed");
+	});
+
+	$( ".wb-menu" ).on( "wb-ready.wb-menu", function( event ) {
+		$("#mb-pnl .mb-menu details").attr("open", "open");
+		$("#mb-pnl .mb-menu details>ul").attr("aria-hidden", "false");
+	});
+};
