95e8338742a3a8d14f3b8d565cc4057c0030ee15
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2023-11-23 09:56:10 -0500

    Update wb-input-range.js

--- fhsa/v4/script/wb-input-range.js
+++ fhsa/v4/script/wb-input-range.js
@@ -6,34 +6,18 @@ document.addEventListener("DOMContentLoaded", function() {
   setValue("paymentYearsOutput", document.getElementById("paymentYears").value); 
 });
 
-document.getElementById("paymentYears").addEventListener("input", function() { 
-  setValue("paymentYearsOutput", this.value); 
-});
-document.addEventListener("DOMContentLoaded", function() { 
-  setValue("savingYearsOutput", document.getElementById("savingYears").value); 
-});
-
-document.getElementById("savingYears").addEventListener("input", function() { 
-  setValue("savingYearsOutput", this.value); 
-});
-
-//currency formatted
-
 document.addEventListener("DOMContentLoaded", function() { 
   setValue("paymentAmountOutput", currencyFormatter.format(document.getElementById("paymentAmount").value)); 
 });
 
-document.getElementById("paymentAmount").addEventListener("input", function() { 
-  setValue("paymentAmountOutput", currencyFormatter.format(this.value)); 
+document.addEventListener("DOMContentLoaded", function() { 
+  setValue("savingYearsOutput", document.getElementById("savingYears").value); 
 });
 
 document.addEventListener("DOMContentLoaded", function() { 
   setValue("savingsDirectAmountOutput", currencyFormatter.format(document.getElementById("savingsDirectAmount").value)); 
 //  setValue("savingsDirectAmountOutput", intCurrencyFormat(document.getElementById("savingsDirectAmount").value)); 
 });
-
-
-//conditional functions
 
 document.addEventListener("DOMContentLoaded", function() { 
   setValue("savingsAmountOutput", currencyFormatter.format(document.getElementById("savingsAmount").value));
@@ -43,15 +27,29 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 });
 
-document.getElementById("savingsAmount").addEventListener("input", function() { 
-  setValue("savingsAmountOutput", currencyFormatter.format(this.value)); 
-  if (parseInt(document.getElementById("savingsAmount").value, 10) < parseInt(document.getElementById("savingsDirectAmount").value, 10)) {
-    setValue("savingsDirectAmountOutput", currencyFormatter.format(document.getElementById("savingsAmount").value)); 
-  }
+document.getElementById("paymentYears").addEventListener("input", function() { 
+  setValue("paymentYearsOutput", this.value); 
 });
 
-document.getElementById("savingsDirectAmount").addEventListener("input", function(event) { 
-  if (parseInt(document.getElementById("savingsDirectAmount").value, 10) <= parseInt(document.getElementById("savingsAmount").value, 10)) {
+document.getElementById("savingYears").addEventListener("input", function() { 
+  setValue("savingYearsOutput", this.value); 
+});
+
+//currency formatted
+document.getElementById("paymentAmount").addEventListener("input", function() { 
+  setValue("paymentAmountOutput", currencyFormatter.format(this.value)); 
+});
+
+//conditional functions
+document.getElementById("savingsAmount").addEventListener("input", function() { 
+  if (parseInt(this.value, 10) <= parseInt(document.getElementById("savingsDirectAmount").value, 10)) {
+    setValue("savingsDirectAmountOutput", currencyFormatter.format(this.value)); 
+  }
+  setValue("savingsAmountOutput", currencyFormatter.format(this.value)); 
+});
+
+document.getElementById("savingsDirectAmount").addEventListener("input", function() { 
+  if (parseInt(this.value, 10) <= parseInt(document.getElementById("savingsAmount").value, 10)) {
     setValue("savingsDirectAmountOutput", currencyFormatter.format(this.value)); 
   }
 });
