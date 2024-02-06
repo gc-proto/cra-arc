be7cd2b35e2c59c46bc9e1d09bb0d3fff09ca892
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2024-02-06 14:39:47 -0500

    Update wb-input-range.js

--- fhsa/v4/script/wb-input-range.js
+++ fhsa/v4/script/wb-input-range.js
@@ -3,7 +3,7 @@
  * @overview Plugin to display current value on an input of type range 
  * @license wet-boew.github.io/wet-boew/License-en.html / wet-boew.github.io/wet-boew/Licence-fr.html
  */
-( function ( $, window, wb ) {
+  ( function ( $, window, wb ) {
   "use strict";
   /*
    * Variable and function definitions.
@@ -21,83 +21,85 @@
        * @param {jQuery Event} event Event that triggered the function call
        */
       init = function( event ) {
-          // Start initialization
-          // returns DOM object = proceed with init
-          // returns undefined = do not proceed with init (e.g., already initialized)
-          var elm = wb.init( event, componentName, selector ),
-              $elm,
-              settings;
-            if ( elm ) {
-                $elm = $( elm );
-                // ... Do the plugin initialisation
-                // Get the plugin JSON configuration set on attribute data-wb-input-range
-                settings = $.extend(
-                    true,
-                    {},
-                    defaults,
-                    window[componentName],
-                    wb.getData( $elm, componentName )
-                );
+        // Start initialization
+        // returns DOM object = proceed with init
+        // returns undefined = do not proceed with init (e.g., already initialized)
+        var elm = wb.init( event, componentName, selector ),
+            $elm,
+            settings;
+          if ( elm ) {
+              $elm = $( elm );
+              // ... Do the plugin initialisation
+              // Get the plugin JSON configuration set on attribute data-wb-input-range
+              settings = $.extend(
+                  true,
+                  {},
+                  defaults,
+                  window[componentName],
+                  wb.getData( $elm, componentName )
+              );
 
-                // Call my custom event
-                $elm.trigger( "wb-input-range", settings );
-                // Identify that initialization has completed
-                wb.ready( $elm, componentName );
+              // Call my custom event
+              $elm.trigger( "wb-input-range", settings );
+              // Identify that initialization has completed
+              wb.ready( $elm, componentName );
+          }
+    }, 
+    doRangeFunc = function( funcName, inputParam ) {
+        let funcArr, fnElem;
+
+        if ( funcName.includes( "." ) === true ) {
+          funcArr = funcName.split( "." );
+          fnElem = {};
+            for ( let i = 0; i <= funcArr.length - 1; i = i + 1 ) {
+                if ( i === 0 ) {
+                    fnElem = funcArr[ i ];
+                } else if ( i === funcArr.length - 1 && typeof window [ fnElem ] !== "undefined" && funcArr[ i ] in window[ fnElem ] && typeof window [ fnElem ][ funcArr [ i ] ] === "function" ) {
+                    return window [ fnElem ][ funcArr [ i ] ] ( inputParam );
+                } else if ( typeof window [ fnElem ] !== "undefined" && funcArr[ i ] in window[ fnElem ] === true ) {
+                    fnElem = fnElem[ funcArr[ i ] ];
+                }
             }
-      }, 
-      doRangeFunc = function( funcName, inputParam ) {
-          let funcArr, fnElem;
-
-          if ( funcName.includes( "." ) === true ) {
-            funcArr = funcName.split( "." );
-            fnElem = {};
-              for ( let i = 0; i <= funcArr.length - 1; i = i + 1 ) {
-                  if ( i === 0 ) {
-                      fnElem = funcArr[ i ];
-                  } else if ( i === funcArr.length - 1 && typeof window [ fnElem ] !== "undefined" && funcArr[ i ] in window[ fnElem ] && typeof window [ fnElem ][ funcArr [ i ] ] === "function" ) {
-                      return window [ fnElem ][ funcArr [ i ] ] ( inputParam );
-                  } else if ( typeof window [ fnElem ] !== "undefined" && funcArr[ i ] in window[ fnElem ] === true ) {
-                      fnElem = fnElem[ funcArr[ i ] ];
-                  }
-              }
           } else if ( typeof window[ funcName ] === "function" ) {
               return window[ funcName ]( inputParam );
           }
           return inputParam;
       }, 
       setRangeValue = function( inputRange ) {
-          inputRange.idArr.forEach( function ( currentId ) {
-              let displayText,
-              elm = document.getElementById( currentId );
+        let outputData, 
+            targetArr = wb.getData( inputRange, "wb-input-range" );
 
-              if ( Object.prototype.hasOwnProperty.call( inputRange, "outputData" ) === true ) {
-                  displayText = doRangeFunc( inputRange.outputData.fn, inputRange.value );
-              } else {
-                  displayText = inputRange.value;
-              }
-              if ( elm.tagName === "TEXTAREA" || elm.tagName === "INPUT" ) {
-                  elm.value = displayText;
-              } else {
-                  elm.innerHTML = displayText;
-              }
-          }, inputRange );
-      };
-  // Add your plugin event handler
-  $document.on( "wb-input-range", selector, function( event, data ) {
-      var elm = event.currentTarget,
-          $inputElm = $( elm ).find( "input[type=range]" ), 
-          outputData = wb.getData( $( elm ).find( ".wb-input-range-display" ), "wb-input-range" ), 
-          inputElm = $inputElm[ 0 ];
-          if ( outputData ) {
-              inputElm.outputData = outputData;
-          }
-          inputElm.idArr = [ ];
-          inputElm.idArr = data.target;
-          setRangeValue( inputElm );
-  });
-  $( ".wb-input-range" ).on( "change input", "input[type=range]", function( event, data ) {
-      setRangeValue( this );
-  });
+        if ( typeof targetArr !== "undefined" && Object.prototype.hasOwnProperty.call( targetArr, "target" ) === true ) {
+            targetArr.target.forEach(function ( currentId ) {
+                let displayText,
+                elm = document.getElementById( currentId );
+
+                outputData = wb.getData( elm, "wb-input-range" );
+                if ( typeof outputData !== "undefined" && Object.prototype.hasOwnProperty.call( outputData, "fn" ) === true ) {
+                    displayText = doRangeFunc( outputData.fn, inputRange.value );
+                } else {
+                     displayText = inputRange.value;
+                }
+                if ( elm.tagName === "TEXTAREA" || elm.tagName === "INPUT" ) {
+                    elm.value = displayText;
+                } else {
+                    elm.innerHTML = displayText;
+                }
+            }, inputRange);
+        }
+    };
+// Add your plugin event handler
+$document.on( "wb-input-range", selector, function( event, data ) {
+    var elm = event.currentTarget,
+        $inputElm = $( elm ).find( "input[type=range]" ), 
+        inputElm = $inputElm[ 0 ];
+        inputElm.params = [ ];
+        inputElm.params = data.target;
+        setRangeValue( inputElm );
+});
+$( ".wb-input-range" ).on("change input", "input[type=range]", function( event, data ) {
+    setRangeValue( this );
+});
   // Bind the init event of the plugin
   $document.on( "timerpoke.wb " + initEvent, selector, init );
   // Add the timer poke to initialize the plugin
